86fb868a86fa6f1bff887e87c187c222413b6a2d
Danidu Muhandiram <muhandiramdanidu@gmail.com>
2026-01-06 10:39:55 +0530

    update chatbot engine to initialize welcome messages and main user options/ safe control for sensitive details

--- chatbot-engine.js
+++ chatbot-engine.js
@@ -1,9 +1,24 @@
-let currentState = "WELCOME";
+//start chatbot
+//include control entry points and handle safety exits
+//called from main.js
 
-function renderState(stateKey) {
-  const state = flows[stateKey];
-  currentState = stateKey;
+function startChatEngine(){
+   //start the chatbot by showing welcome messages
+   addMessage("Welcome to Palo accounting firm.");
+   addMessage("How can we assist you today?");
 
-  addBotMessage(state.message);
-  showOptions(state.options);
+   //display the main options to the user
+   showOptions([
+      { text: "Tax Services", action: taxFlow },
+      { text: "Accounting & Bookkeeping", action: accountingFlow },
+      { text: "Talk to an Accountant", action: contactFlow }
+    ]);
 }
+
+//safety exit function to end the chat (for sensitive detais)
+function unasfeQuestion(){
+    clearOptions();
+    addMessage("This requires professional review. Please consult our professionals directly.");
+
+}
+
